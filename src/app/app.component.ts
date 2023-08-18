0b14a45a71cedc06768112795c264edf67f11935
RamonPC <rgzodiac@gmail.com>
2023-08-18 12:22:19 +0200

    #7 - Migrate to Standalone Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,24 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 import { AccountService } from './_services';
 import { User } from './_models';
+import { AlertComponent } from './_components/alert.component';
 
-@Component({ selector: 'app-root', templateUrl: 'app.component.html' })
+@Component({
+    selector: 'app-root', templateUrl: 'app.component.html',
+    standalone: true,
+    imports: [NgIf, RouterOutlet, RouterLink, RouterLinkActive, AlertComponent]
+})
 export class AppComponent {
-  user?: User | null;
+    user?: User | null;
 
-  constructor(private accountService: AccountService) {
-    this.accountService.user.subscribe(x => this.user = x);
-  }
+    constructor(private accountService: AccountService) {
+        this.accountService.user.subscribe(x => this.user = x);
+    }
 
-  logout() {
-    this.accountService.logout();
-  }
+    logout() {
+        this.accountService.logout();
+    }
 }
--- src/app/users/list.component.ts
+++ src/app/users/list.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+import { RouterLink } from '@angular/router';
 import { first } from 'rxjs/operators';
 
 import { AccountService } from '@app/_services';
 
-@Component({ templateUrl: 'list.component.html' })
+@Component({
+    templateUrl: 'list.component.html',
+    standalone: true,
+    imports: [RouterLink, NgFor, NgIf]
+})
 export class ListComponent implements OnInit {
     users?: any[];
 
